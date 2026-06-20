e2b1b1546204848ad573f7a95bb67e4ef1b0377d
iliahelianoori-art <iliahelianoori@gmail.com>
2026-06-20 14:05:25 +0330

    Add files via upload

--- Calculator_i.py
+++ Calculator_i.py
@@ -9,7 +9,6 @@ root.configure(bg="#1c1c1c")
 root.resizable(False, False)
 
 #create global expression
-global expression, mod
 expression = ""
 mod = "Standard"
 
@@ -36,10 +35,9 @@ history = tk.Text(
     relief="flat",
 )
 history.grid(row=1, column=0, columnspan=4, padx=10, pady=(0, 15), sticky="nsew")
-
 #functions
 def buttons():
-
+    btn_dict = {}
     btn_c = tk.Button(
         root,
         text="C",
@@ -49,10 +47,11 @@ def buttons():
         relief="flat",
         font=("Segoe UI", 14, "bold"),
         width=5,
-        height=2
+        height=2,
     )
+    btn_c["command"] = lambda b=btn_c: press(b)
     btn_c.grid(row=6, column=3, padx=5, pady=5, sticky="nsew")
-    btn_c.bind("<Button-1>", press)
+    btn_dict["btn_c"] = btn_c
 
     buttons_list = [
         ["7", "8", "9", "/"],
@@ -78,12 +77,14 @@ def buttons():
                 text=text,
                 fg=fg,
                 bg=bg,
-                activebackground="#555555",
+                activebackground=bg,
+                activeforeground=fg,
                 relief="flat",
                 font=("Segoe UI", 14, "bold"),
                 width=5,
-                height=2
+                height=2,
             )
+            btn["command"] = lambda b=btn: press(b)
 
             btn.grid(
                 row=i + 2,
@@ -92,8 +93,21 @@ def buttons():
                 pady=5,
                 sticky="nsew"
             )
+            if text.isdigit():
+                btn_dict[f"btn_{text}"] = btn
+            elif text == ".":
+                btn_dict["btn_point"] = btn
+            elif text == "x":
+                btn_dict["btn_mul"] = btn
+            elif text == "/":
+                btn_dict["btn_div"] = btn
+            elif text == "-":
+                btn_dict["btn_min"] = btn
+            elif text == "+":
+                btn_dict["btn_plus"] = btn
+            elif text == "=":
+                btn_dict["btn_eq"] = btn
 
-            btn.bind("<Button-1>", press)
     btn_setmode = tk.Button(
                   root,
                   text="Standard",
@@ -113,10 +127,13 @@ def buttons():
         sticky="nsew"
 
     )
-    btn_setmode.bind("<Button-1>", press)
-def press(event):
+    btn_dict["btn_mod"] = btn_setmode
+    btn_setmode["command"] = lambda b=btn_setmode: press(b)
+    root.bind("<Key>", lambda event: key_event(event, btn_dict))
+
+def press(btn):
+    btn_text = btn.cget("text")
     global mod, expression
-    btn_text = event.widget.cget("text")
     if btn_text.isdigit() or btn_text == ".":
         numbers(btn_text)
     elif btn_text == "C":
@@ -127,13 +144,13 @@ def press(event):
         else:
             equal_scientific()
     elif btn_text == "Standard":
-        event.widget.config(text="Scientific")
+        btn.config(text="Scientific")
         mod = "Scientific"
         entry.delete(0, tk.END)
         expression = ""
         history.delete("1.0", tk.END)
     elif btn_text == "Scientific":
-        event.widget.config(text="Standard")
+        btn.config(text="Standard")
         entry.delete(0, tk.END)
         expression = ""
         history.delete("1.0", tk.END)
@@ -168,12 +185,15 @@ def numbers(btn_text):
             if not left:
                 if not "." in entry.get():
                     entry.insert(tk.END, btn_text)
+                    history.insert(tk.END, btn_text)
             else:
                 if not right:
                     entry.insert(tk.END, "0.")
+                    history.insert(tk.END, "0.")
                 else:
                     if not "." in right:
                         entry.insert(tk.END, btn_text)
+                        history.insert(tk.END, btn_text)
 
     if entry.get() == ".":
         entry.delete(0, tk.END)
@@ -251,12 +271,42 @@ def equal_standard():
         entry.delete(0, tk.END)
         entry.insert(tk.END, result)
         history.insert(tk.END, f"={result}\n{result}")
+def key_event(event, btn_dict):
+    check = True
+    btn = None
+    if event.char.isdigit():
+        btn = btn_dict[f"btn_{event.char}"]
+    elif event.char == ".":
+        btn = btn_dict["btn_point"]
+    elif event.char == "+":
+        btn = btn_dict["btn_plus"]
+    elif event.char == "-":
+        btn = btn_dict["btn_min"]
+    elif event.char == "*":
+        btn = btn_dict["btn_mul"]
+    elif event.char == "/":
+        btn = btn_dict["btn_div"]
+    elif event.keysym == "Return":
+        btn = btn_dict["btn_eq"]
+    elif event.char == "c":
+        btn = btn_dict["btn_c"]
+    elif event.keysym == "space":
+        btn = btn_dict["btn_mod"]
+    else:
+        check = False
+    if check:
+        btn.config(relief="sunken")
+        press(btn)
+        btn.after(100, lambda: btn.config(relief="flat"))
+
 for i in range(7):
     root.grid_rowconfigure(i, weight=1)
 
 for j in range(4):
     root.grid_columnconfigure(j, weight=1)
 
+
 #run
 buttons()
+
 root.mainloop()
